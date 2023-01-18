b0a0edf6b7ede8c28b7108e40e6316b95f272e2b
IceTeaXXD <13521024@std.stei.itb.ac.id>
2023-01-18 15:26:08 +0700

    Program initialization

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Tucil1-13521024
 Tucil 1 STIMA 13521024 Ahmad Nadil
+
+## Cara Compile Program
+1. Buka Terminal pada Directory File ini
+
+2. Jalankan program dengan menuliskan command sebagai berikut : 
+
+`g++ -o bin/main.exe src/main.cpp`
+
+`bin/main.exe`
---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main(){
+    // Inisiasi Variabel
+    string kartu[] = {"A","2","3","4","5","6","7","8","9","10","J","Q","K"};
+    int nilai_kartu[] = {1,2,3,4,5,6,7,8,9,10,11,12,13};
+
+    // Menu Input atau Random Kartu
+    bool run = true;
+
+    // PRINT THE SPLASH SCREEN
+                                                                                       
+    cout << "  ___    _  _            _______.  ______    __      ____    ____  _______ .______      " << endl;
+    cout << " |__ \\  | || |          /       | /  __  \\  |  |     \\   \\  /   / |   ____||   _  \\     " << endl;
+    cout << "    ) | | || |_        |   (----`|  |  |  | |  |      \\   \\/   /  |  |__   |  |_)  |    " << endl;
+    cout << "   / /  |__   _|        \\   \\    |  |  |  | |  |       \\      /   |   __|  |      /     " << endl;
+    cout << "  / /_     | |      .----)   |   |  `--'  | |  `----.   \\\\    /    |  |____ |  |\\\\  \\\\----." << endl;
+    cout << " |____|    |_|      |_______/     \\\\______/  |_______|    \\\\__/     |_______|| _| `._____|" << endl;
+    cout << "                             BY 13521024 - AHMAD NADIL                                         " << endl;
+
+        int pilihan;
+        cout << "===== MENU INPUT KARTU =====" << endl;
+        cout << "1. Input Kartu" << endl;
+        cout << "2. Random Kartu" << endl;
+}
