34f256c9f7843166eeea1598170f1f4a581335b8
IceTeaXXD <13521024@std.stei.itb.ac.id>
2023-01-19 11:37:01 +0700

    added permutation (24 validator not finished)

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,16 @@
 #include <vector>
 using namespace std;
 
+string inttostr(int num){
+    // change int to string
+    string str = "";
+    while (num > 0){
+        str = (char)(num % 10 + 48) + str;
+        num /= 10;
+    }
+    return str;
+}
+
 int strtoint(string c){
     /*
     I.S. Menerima sebuah string
@@ -55,6 +65,12 @@ int strtoint(string c){
     }
 }
 
+void swap(int *a, int *b){
+    // Fungsi untuk menukar 2 angka
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
 void check24 (int nums[4], vector<string> *hasil, int *count){
     // Fungsi untuk mengecek apakah ada 4 angka yang dapat menghasilkan 24
     int a = nums[0];
@@ -64,129 +80,149 @@ void check24 (int nums[4], vector<string> *hasil, int *count){
 
     // Operasi komutatif
     if (a+b+c+d == 24){
-        hasil->push_back(to_string(a) + " + " + to_string(b) + " + " + to_string(c) + " + " + to_string(d));
+        hasil->push_back(inttostr(a) + " + " + inttostr(b) + " + " + inttostr(c) + " + " + inttostr(d));
         *count += 1;
     }
     if (a*b*c*d == 24){
-        hasil->push_back(to_string(a) + " * " + to_string(b) + " * " + to_string(c) + " * " + to_string(d));
+        hasil->push_back(inttostr(a) + " * " + inttostr(b) + " * " + inttostr(c) + " * " + inttostr(d));
         *count += 1;
     }
 
     // 2 '+' dan 1 '-'
     if (a+b+c-d == 24){
-        hasil->push_back(to_string(a) + " + " + to_string(b) + " + " + to_string(c) + " - " + to_string(d));
+        hasil->push_back(inttostr(a) + " + " + inttostr(b) + " + " + inttostr(c) + " - " + inttostr(d));
         *count += 1;
     }
 
     // 2 '+' dan 1 '*'
     if (a*b+c+d == 24){
-        hasil->push_back(to_string(a) + " * " + to_string(b) + " + " + to_string(c) + " + " + to_string(d));
+        hasil->push_back(inttostr(a) + " * " + inttostr(b) + " + " + inttostr(c) + " + " + inttostr(d));
         *count += 1;
     }
     if (a*(b+c)+d == 24){
-        hasil->push_back(to_string(a) + " * (" + to_string(b) + " + " + to_string(c) + ") + " + to_string(d));
+        hasil->push_back(inttostr(a) + " * (" + inttostr(b) + " + " + inttostr(c) + ") + " + inttostr(d));
     }
     if (a*(b+c+d) == 24){
-        hasil->push_back(to_string(a) + " * (" + to_string(b) + " + " + to_string(c) + " + " + to_string(d) + ")");
+        hasil->push_back(inttostr(a) + " * (" + inttostr(b) + " + " + inttostr(c) + " + " + inttostr(d) + ")");
         *count += 1;
     }
 
     // 1 '+' dan 2 '*'
     if ((a*b*c)+d == 24){
-        hasil->push_back("(" + to_string(a) + " * " + to_string(b) + " * " + to_string(c) + ") + " + to_string(d));
+        hasil->push_back("(" + inttostr(a) + " * " + inttostr(b) + " * " + inttostr(c) + ") + " + inttostr(d));
         *count += 1;
     }
     if(a*b*(c+d) == 24){
-        hasil->push_back(to_string(a) + " * " + to_string(b) + " * (" + to_string(c) + " + " + to_string(d) + ")");
+        hasil->push_back(inttostr(a) + " * " + inttostr(b) + " * (" + inttostr(c) + " + " + inttostr(d) + ")");
         *count += 1;
     }
     if((a*b)+(c*d)){
-        hasil->push_back("(" + to_string(a) + " * " + to_string(b) + ") + (" + to_string(c) + " * " + to_string(d) + ")");
+        hasil->push_back("(" + inttostr(a) + " * " + inttostr(b) + ") + (" + inttostr(c) + " * " + inttostr(d) + ")");
         *count += 1;
     }
 
     // 1 '-' dan 2 '*'
     if((a*b*c)-d == 24){
-        hasil->push_back("(" + to_string(a) + " * " + to_string(b) + " * " + to_string(c) + ") - " + to_string(d));
+        hasil->push_back("(" + inttostr(a) + " * " + inttostr(b) + " * " + inttostr(c) + ") - " + inttostr(d));
         *count += 1;
     }
     if(a*b*(c-d) == 24){
-        hasil->push_back(to_string(a) + " * " + to_string(b) + " * (" + to_string(c) + " - " + to_string(d) + ")");
+        hasil->push_back(inttostr(a) + " * " + inttostr(b) + " * (" + inttostr(c) + " - " + inttostr(d) + ")");
         *count += 1;
     }
     if((a*b)-(c*d) == 24){
-        hasil->push_back("(" + to_string(a) + " * " + to_string(b) + ") - (" + to_string(c) + " * " + to_string(d) + ")");
+        hasil->push_back("(" + inttostr(a) + " * " + inttostr(b) + ") - (" + inttostr(c) + " * " + inttostr(d) + ")");
         *count += 1;
     }
 
     // 1 '+' dan 1 '-' dan 1 '*'
     if(a*b+c-d == 24){
-        hasil->push_back(to_string(a) + " * " + to_string(b) + " + " + to_string(c) + " - " + to_string(d));
+        hasil->push_back(inttostr(a) + " * " + inttostr(b) + " + " + inttostr(c) + " - " + inttostr(d));
         *count += 1;
     }
     if(a*(b+c)-d == 24){
-        hasil->push_back(to_string(a) + " * (" + to_string(b) + " + " + to_string(c) + ") - " + to_string(d));
+        hasil->push_back(inttostr(a) + " * (" + inttostr(b) + " + " + inttostr(c) + ") - " + inttostr(d));
         *count += 1;
     }
     if(a*(b-c)+d == 24){
-        hasil->push_back(to_string(a) + " * (" + to_string(b) + " - " + to_string(c) + ") + " + to_string(d));
+        hasil->push_back(inttostr(a) + " * (" + inttostr(b) + " - " + inttostr(c) + ") + " + inttostr(d));
         *count += 1;
     }
     if(a*(b+c-d) == 24){
-        hasil->push_back(to_string(a) + " * (" + to_string(b) + " + " + to_string(c) + " - " + to_string(d) + ")");
+        hasil->push_back(inttostr(a) + " * (" + inttostr(b) + " + " + inttostr(c) + " - " + inttostr(d) + ")");
         *count += 1;
     }
     if(a*b-(c+d) == 24){
-        hasil->push_back(to_string(a) + " * " + to_string(b) + " - (" + to_string(c) + " + " + to_string(d) + ")");
+        hasil->push_back(inttostr(a) + " * " + inttostr(b) + " - (" + inttostr(c) + " + " + inttostr(d) + ")");
         *count += 1;
     }
 
     // 1 '*' dan 1 '/' dan 1 '+'
     if (a*b == (24-d) * c){ // (a*b/c)+d
-        hasil->push_back("(" + to_string(a) + " * " + to_string(b) + " / " + to_string(c) + ") + " + to_string(d));
+        hasil->push_back("(" + inttostr(a) + " * " + inttostr(b) + " / " + inttostr(c) + ") + " + inttostr(d));
         *count += 1;
     }
     if(((a*b)+c) == 24*d){ // ((a*b)+c)/d
-        hasil->push_back("((" + to_string(a) + " * " + to_string(b) + ") + " + to_string(c) + ") / " + to_string(d));
+        hasil->push_back("((" + inttostr(a) + " * " + inttostr(b) + ") + " + inttostr(c) + ") / " + inttostr(d));
         *count += 1;
     }
     if((a+b)*c == 24*d){ // ((a+b)*c)/d
-        hasil->push_back("((" + to_string(a) + " + " + to_string(b) + ") * " + to_string(c) + ") / " + to_string(d));
+        hasil->push_back("((" + inttostr(a) + " + " + inttostr(b) + ") * " + inttostr(c) + ") / " + inttostr(d));
         *count += 1;
     }
     if(a*b == 24*(c+d)){ // (a*b)/(c+d)
-        hasil->push_back("(" + to_string(a) + " * " + to_string(b) + ") / (" + to_string(c) + " + " + to_string(d) + ")");
+        hasil->push_back("(" + inttostr(a) + " * " + inttostr(b) + ") / (" + inttostr(c) + " + " + inttostr(d) + ")");
         *count += 1;
     }
 
     // 1 '*' dan 1 '/' dan 1 '-'
     if(a*b == (24 + d)*c){ // (a*b/c)-d
-        hasil->push_back("(" + to_string(a) + " * " + to_string(b) + " / " + to_string(c) + ") - " + to_string(d));
+        hasil->push_back("(" + inttostr(a) + " * " + inttostr(b) + " / " + inttostr(c) + ") - " + inttostr(d));
         *count += 1;
     }
     if(((a*b)-c) == 24*d){ // ((a*b)-c)/d
-        hasil->push_back("((" + to_string(a) + " * " + to_string(b) + ") - " + to_string(c) + ") / " + to_string(d));
+        hasil->push_back("((" + inttostr(a) + " * " + inttostr(b) + ") - " + inttostr(c) + ") / " + inttostr(d));
         *count += 1;
     }
     if((a-b)*c == 24*d){ // ((a-b)*c)/d
-        hasil->push_back("((" + to_string(a) + " - " + to_string(b) + ") * " + to_string(c) + ") / " + to_string(d));
+        hasil->push_back("((" + inttostr(a) + " - " + inttostr(b) + ") * " + inttostr(c) + ") / " + inttostr(d));
         *count += 1;
     }
     if(a*b == 24*(c-d)){ // (a*b)/(c-d)
-        hasil->push_back("(" + to_string(a) + " * " + to_string(b) + ") / (" + to_string(c) + " - " + to_string(d) + ")");
+        hasil->push_back("(" + inttostr(a) + " * " + inttostr(b) + ") / (" + inttostr(c) + " - " + inttostr(d) + ")");
         *count += 1;
     }
 
     // 2 '*' dan 1 '/'
     if(a*b*c == 24*d){ // a*b*c/d
-        hasil->push_back(to_string(a) + " * " + to_string(b) + " * " + to_string(c) + " / " + to_string(d));
+        hasil->push_back(inttostr(a) + " * " + inttostr(b) + " * " + inttostr(c) + " / " + inttostr(d));
         *count += 1;
     }
     if(a*b == 24*c*d){ //a*b/(c*d)
-        hasil->push_back(to_string(a) + " * " + to_string(b) + " / (" + to_string(c) + " * " + to_string(d) + ")");
+        hasil->push_back(inttostr(a) + " * " + inttostr(b) + " / (" + inttostr(c) + " * " + inttostr(d) + ")");
         *count += 1;
     }
 }
+
+void permutations(int nums[4], int start, int end, vector<string> *hasil, int *count){
+    // Fungsi untuk mendapat semua kemungkinan dari 4 angka
+    if (start == end){
+        check24(nums, hasil, count);
+    }
+    else{
+        for (int i = start; i <= end; i++){
+            swap(&nums[start], &nums[i]);
+            permutations(nums, start+1, end, hasil, count);
+            swap(&nums[start], &nums[i]);
+        }
+    }
+}
+
+// fungsi untuk mendapat semua kemungkinan dari 4 angka
+void permute(int nums[4], vector<string> *hasil, int *count){
+    permutations(nums, 0, 3, hasil, count);
+}
+
 
 int main(){
     /* SPLASH SCREEN */
@@ -209,6 +245,8 @@ int main(){
     int length_kartu = 4;
     int nilai_kartu[4];    
     string arr_kartu[] = {"A","2","3","4","5","6","7","8","9","10","J","Q","K"};
+    vector<string> hasil;
+    int count = 0;
 
     while (run){
         cout << "===== MENU UTAMA =====" << endl;
@@ -238,6 +276,20 @@ int main(){
                     }
                     else if (i == length_kartu - 1){
                         valid = true;
+                    }
+                }
+
+                // cek nilai 24
+                permute(nilai_kartu, &hasil, &count);
+
+                // print hasil
+                if (count == 0){
+                    cout << "Tidak ada solusi" << endl;
+                }
+                else{
+                    cout << count << " solusi ditemukan" << endl;
+                    for (int i = 0; i < hasil.size(); i++){
+                        cout << hasil[i] << endl;
                     }
                 }
             }
